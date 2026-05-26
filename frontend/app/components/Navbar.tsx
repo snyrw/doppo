7a41fb4a52eaea872368d20a87e02bb33b45aa78
Will Snyder <77564706+snyrw@users.noreply.github.com>
2026-05-25 20:52:12 -0700

    final modal removal stuff (for now)

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -129,7 +129,7 @@ export default function Navbar({ actions, onAddCredits }: { actions?: React.Reac
             letterSpacing: "-0.01em",
           }}
         >
-          Doppo
+          doppo
         </span>
       </Link>
 
